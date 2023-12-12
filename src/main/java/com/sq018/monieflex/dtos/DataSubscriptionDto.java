0ab682374d1af48ad62ac360a5164bb3ed9b1d89
mac <mac@macs-MacBook-Air.local>
2023-12-12 16:22:50 +0100

    Merged conflict resolved

--- src/main/java/com/sq018/monieflex/dtos/DataSubscriptionDto.java
+++ src/main/java/com/sq018/monieflex/dtos/DataSubscriptionDto.java
@@ -2,7 +2,7 @@ package com.sq018.monieflex.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record DataSubscriptionDto (
+public record DataSubscriptionDto(
         String serviceID,
         String billersCode,
         @JsonProperty("variation_code")
--- src/main/java/com/sq018/monieflex/services/providers/VtPassService.java
+++ src/main/java/com/sq018/monieflex/services/providers/VtPassService.java
@@ -1,17 +1,23 @@
 package com.sq018.monieflex.services.providers;
 
 
-import com.sq018.monieflex.exceptions.MonieFlexException;
-import com.sq018.monieflex.payloads.vtpass.VtpassTVariationResponse;
+import com.sq018.monieflex.dtos.DataSubscriptionDto;
+import com.sq018.monieflex.dtos.VtpassDataSubscriptionDto;
+import com.sq018.monieflex.entities.transactions.Transaction;
+import com.sq018.monieflex.enums.TransactionStatus;
+import com.sq018.monieflex.payloads.vtpass.VtpassDataSubscriptionResponse;
 import com.sq018.monieflex.utils.VtpassEndpoints;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+import java.util.UUID;
 
 
 @Service
@@ -47,16 +53,43 @@ public class VtPassService {
         return headers;
     }
 
-    public VtpassTVariationResponse getTvVariations(String code) {
-        HttpEntity<Object> entity = new HttpEntity<>(vtPassGetHeader());
-        var response = restTemplate.exchange(
-                VtpassEndpoints.VARIATION_URL(code), HttpMethod.GET, entity,
-                VtpassTVariationResponse.class
+
+    public String generateRequestId() {
+        StringBuilder result = new StringBuilder();
+        String date = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE);
+        result.append(date.replaceAll("-", ""));
+        result.append(LocalDateTime.now().getHour());
+        result.append(LocalDateTime.now().getMinute());
+        result.append(UUID.randomUUID().toString().substring(0, 15));
+        return result.toString();
+    }
+
+    @SneakyThrows
+    public Transaction dataSubscription(DataSubscriptionDto dataSubscriptionDto, Transaction transaction) {
+        VtpassDataSubscriptionDto vtData = new VtpassDataSubscriptionDto(
+                transaction.getReference(),
+                dataSubscriptionDto.serviceID(),
+                dataSubscriptionDto.billersCode(),
+                dataSubscriptionDto.variationCode(),
+                dataSubscriptionDto.amount(),
+                dataSubscriptionDto.phone()
         );
-        if(response.getStatusCode().is2xxSuccessful()) {
-            return response.getBody();
+        HttpEntity<VtpassDataSubscriptionDto> buyBody = new HttpEntity<>(vtData, vtPassPostHeader());
+        var buyResponse = restTemplate.postForEntity(
+                VtpassEndpoints.PAY,
+                buyBody, VtpassDataSubscriptionResponse.class
+        );
+        if(Objects.requireNonNull(buyResponse.getBody()).getResponseDescription().toLowerCase().contains("success")) {
+            var reference = buyResponse.getBody().getToken() != null
+                    ? buyResponse.getBody().getToken()
+                    : buyResponse.getBody().getExchangeReference();
+            transaction.setNarration("Data Billing");
+            transaction.setReference(buyResponse.getBody().getRequestId());
+            transaction.setProviderReference(reference);
+            transaction.setStatus(TransactionStatus.SUCCESSFUL);
         } else {
-            throw new MonieFlexException("Request failed");
+            transaction.setStatus(TransactionStatus.FAILED);
         }
+        return transaction;
     }
 }
