512e447a6dccc8f8d96645f219e07d5d6809b748
Mervo <evaristusadimonyemma@gmail.com>
2023-12-20 15:51:57 +0100

    Updated Transfer to Other Banks

--- src/main/java/com/sq018/monieflex/payloads/flutterwave/FLWTransferResponse.java
+++ src/main/java/com/sq018/monieflex/payloads/flutterwave/FLWTransferResponse.java
@@ -1,12 +1,10 @@
 package com.sq018.monieflex.payloads.flutterwave;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
 public class FLWTransferResponse {
